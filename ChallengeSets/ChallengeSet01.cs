aea18980bbf524cb8788cd56586541c0282d4190
Joel Curry <joelmattcurry@gmail.com>
2020-03-14 16:01:31 -0500

    updated multiply

--- ChallengeSets/ChallengeSet01.cs
+++ ChallengeSets/ChallengeSet01.cs
@@ -20,7 +20,7 @@ namespace ChallengeSets
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            return number1 - number2;
         }
 
         public long Multiply(long factor1, long factor2)
