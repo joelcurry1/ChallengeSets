82cfca6b599731158f8bccd8751c68228626e3ff
Joel Curry <joelmattcurry@gmail.com>
2020-03-29 21:58:21 -0500

    updated challenge set 2

--- ChallengeSets/ChallengeSet02.cs
+++ ChallengeSets/ChallengeSet02.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace ChallengeSets
 {
@@ -7,52 +9,139 @@ namespace ChallengeSets
     {
         public bool CharacterIsALetter(char c)
         {
-            throw new NotImplementedException();
+            return char.IsLetter(c);
         }
 
         public bool CountOfElementsIsEven(string[] vals)
         {
-            throw new NotImplementedException();
+            if (vals.Length % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public bool IsNumberEven(int number)
         {
-            throw new NotImplementedException();
+            if (number % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public bool IsNumberOdd(int num)
         {
-            throw new NotImplementedException();
+            if (num % 2 == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
         }
 
         public double SumOfMinAndMax(IEnumerable<double> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null || numbers.Count() == 0)
+            {
+                return 0;
+            }
+            double maxValue = 1000000.0;
+            double minValue = 0;
+            foreach (double duh in numbers.ToList())
+            {
+                if (duh < maxValue)
+                {
+                    maxValue = duh;
+                }
+                if (duh > minValue)
+                {
+                    minValue = duh;
+                }
+            }
+            return minValue + maxValue;
         }
 
         public int GetLengthOfShortestString(string str1, string str2)
         {
-            throw new NotImplementedException();
+            if (str1.Length < str2.Length)
+            {
+                return str1.Length;
+            }
+            return str2.Length;
         }
 
         public int Sum(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers != null)
+            {
+                int[] arr = new int[] { };
+                int sum = 0;
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    sum += numbers[i];
+                }
+                return sum;
+            }
+            return 0;
         }
-
         public int SumEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers != null)
+            {
+                int[] arr = new int[] { };
+                int sumevens = 0;
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    if (numbers[i] % 2 == 0)
+                    {
+                        sumevens += numbers[i];
+                    }
+                }
+                return sumevens;
+            }
+            return 0;
         }
 
         public bool IsSumOdd(List<int> numbers)
         {
-            throw new NotImplementedException();
+            
+            if(numbers != null)
+            {
+                int sum = 0;
+                for (int i = 0; i < numbers.Count; i++)
+                {
+
+                    sum += numbers[i];
+                }
+                if (sum % 2 == 0)
+                {
+                    return false;
+                }
+                return true;
+            }
+            return false;
         }
 
         public long CountOfPositiveOddsBelowNumber(long number)
         {
-            throw new NotImplementedException();
+            int oddsCounter = 0;
+            for (int i = 0; i < number; i++)
+            {
+                if(i % 2 != 0)
+                {
+                    oddsCounter++;
+                }
+            }
+            return oddsCounter;
         }
     }
 }
